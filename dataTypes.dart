d967e4af8dc41ee83a3bb48da5f77d1934c49838
katlegojt <katlego209@gmail.com>
2023-02-07 17:24:36 +0200

    first commit

---
+++ dataTypes.dart
@@ -0,0 +1,37 @@
+main() {
+  //double example
+  double pi = 3.14;
+
+  //boolean example
+  bool isTrue = true;
+  bool isFalse = false;
+
+// interger example
+  int age = 29;
+
+// string example
+  String name = "katlego";
+
+  // list example
+  List cars = [
+    "Toyota",
+    "Mazda",
+    "Ford",
+  ];
+
+  //Runes example
+  var laugh_symbol = '\u{1f600}';
+  var heart_symbol = '\u2665';
+
+//example of map
+  var details = {'Username': 'Kat', 'Password': 'pass@123'};
+
+//printing statements
+  print("My name is " + name + " my age is $age");
+  print("The value of PI is $pi");
+  print("My Login details are $details");
+  print("list of cars $cars");
+  print(laugh_symbol + heart_symbol);
+  print(isFalse);
+  print(isTrue);
+}
